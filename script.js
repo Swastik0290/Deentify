4f189715acd8b32c20bbda7d2445bbe35d06ed85
Swastik0290 <157957341+Swastik0290@users.noreply.github.com>
2024-03-07 02:05:22 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+// btn.addEventListener('submit',(e)=>{
+//     console.log(nameC);
+// })
+
+var form = document.getElementById('send_data');
+
+
+form.onsubmit = (e) => {
+    var enteredName = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phoneNo = document.getElementById('phone').value;
+    var subject = document.getElementById('subject').value;
+    var message = document.getElementById('message').value;
+
+    const data = {
+        name: enteredName,
+        email: email,
+        phoneNo: phoneNo,
+        subject: subject,
+        message: message
+    }
+
+    e.preventDefault()
+    console.log(data);
+}
